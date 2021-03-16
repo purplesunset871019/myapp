47564bb25e27bdb58d62c441df0dbea595a00105
purplesunset871019 <32476279+purplesunset871019@users.noreply.github.com>
2021-03-16 17:50:23 +0800

    首頁剩右下角

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '快肯系統整合股份有限公司' });
 });
 
 module.exports = router;
