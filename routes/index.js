e304425f8794f2decb7bbd6c81ed0e4c33f73b2a
purplesunset871019 <32476279+purplesunset871019@users.noreply.github.com>
2021-03-25 15:24:00 +0800

    3/25
    company用到一半，暫時停止

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
-var router = express.Router();
 
+var router = express.Router();
+console.log("router.get");
+console.log(router.get);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '快肯系統整合股份有限公司' });
+  //console.log("indexres");
+  //console.log(res);
+  //console.log("indexrender");
+  //console.log(render);
 });
 
 module.exports = router;
